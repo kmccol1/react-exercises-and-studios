9086675134da881674e6b14b95f0abc1e438ce23
Kyle McColgan <kdmbus21@gmail.com>
2024-03-11 22:30:24 -0500

    Upload chapter twenty nine studio parts two and three.

--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,9 @@
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+//    return <p>No content is here!</p>;
+   return (<ul>
+               <li>Empty dishwasher</li>
+               <li>Complete prep work</li>
+               <li>Buy groceries</li>
+           </ul>
+   );
 }
---
+++ part1/exercises/src/components/HobbyLinks.jsx
@@ -0,0 +1,5 @@
+export default function HobbyLinks()
+{
+    let hobbyLinks = ['https://thebeatva.iheart.com/', 'https://www.ebay.com/'];
+    return (   <div><a href = {hobbyLinks[0]}>Music</a> <a href = {hobbyLinks[1]}>Equipment</a></div>);
+}
