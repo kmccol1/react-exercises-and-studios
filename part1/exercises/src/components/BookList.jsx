fdb4e1e0c59dfecbe346a9f5c3cc9fd504560fef
Kyle McColgan <kdmbus21@gmail.com>
2024-03-11 21:27:45 -0500

    Upload chapter twenty nine studio part one code.

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Book List:";
+   let book1 = "https://yesterdaysmuse.cdn.bibliopolis.com/pictures/2312512.jpg?auto=webp&v=1706726301";
+   let book2 = "https://m.media-amazon.com/images/I/81GjCVSEDAL._AC_UF1000,1000_QL80_.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/61Pgdn8Ys-L._AC_UF1000,1000_QL80_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="A Chronicle of a Death Foretold" />
+         <img src={book2} alt="The Stranger" />
+         <img src={book3} alt="Algorithms" />
       </div>      
    );
 }
