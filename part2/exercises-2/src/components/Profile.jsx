86e3d57bdbecd186e3a9620085fcc1bdb14a85d0
Kyle McColgan <kdmbus21@gmail.com>
2024-03-13 01:37:14 -0500

    Upload chapter thirty textbook exercise code.

--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -1,14 +1,34 @@
 import './styles.css';
 import Button from './Button';
+import oceans from './oceans.json';
 
 
 function Profile()
 {
+
+   const listItem = oceans.map(ocean =>
+                               <div key= {ocean.id} className={`${ocean.fishCheck === "true" ? "isAFish" : "profile"}`}>
+                                   <h1> {ocean.name} </h1>
+                                    <h5>Fun Facts:</h5>
+                                    <ol>
+                                       <li>{ocean.fact1}</li>
+                                       <li>{ocean.fact2}</li>
+                                       <li>{ocean.fact3}</li>
+                                    </ol>
+                                   <img src={ocean.image} alt={ocean.name}
+                                   className = "img"/>
+                                   <Button />
+                               </div>
+   );
+
+
+
+
+
    return(
-      <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
-         <Button />
-      </>  
+         <ul>
+           {listItem}
+         </ul>
    );
 }
 
